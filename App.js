4ae1f812d0631c741187f732cf089ec9e2a74b20
Dmitry Dergunov <dmitrynuc@gmail.com>
2020-12-28 14:57:19 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+// import { createStackNavigator } from 'react-navigation-stack';
+// import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 export default function App() {
   return (
