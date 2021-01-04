841b5a7f8fc568697ab6f445066fb926f7d0a108
Dmitry Dergunov <dmitrynuc@gmail.com>
2021-01-03 20:34:50 -0500

    signup action, storing token in async storage

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,3 +1,4 @@
+import AsyncStorage from '@react-native-community/async-storage';
 import createDataContext from './createDataContext';
 import trackerApi from '../api/tracker'; 
 
@@ -5,26 +6,33 @@ const authReducer = (state, action) => {
     switch (action.type) {
         case 'add_error':
             return { ...state, errorMessage: action.payload };
+        case 'signup':
+            return { token: action.payload, errorMessage: '' };
         default: 
             return state;
     }
 };
  
-const signup = (dispatch) => {
-    return async ({ email, password }) => {
-        //make api request
-        try {
-            const response = await trackerApi.post('/signup', { email, password });
-            console.log(response.data);
-        } catch(err) {
-            console.log(err.response.data);
-            dispatch({ 
-                type: 'add_error',
-                payload: 'Something went wrong with sign up'
-            });
-        }
-    };
-}
+const signup = (dispatch) => async ({ email, password }) => {
+    //make api request
+    try {
+        const response = await trackerApi.post('/signup', { email, password });
+        //storing token on the device 
+        await AsyncStorage.setItem('token', response.data.token);
+        dispatch({ 
+            type: 'signup',
+            payload: response.data.token
+        });
+        //navigate to mainFlow
+
+    } catch(err) {
+        console.log(err.response.data);
+        dispatch({ 
+            type: 'add_error',
+            payload: 'Something went wrong with sign up'
+        });
+    }    
+};
 
 const signin = (dispatch) => {
     return ({ email, password }) => {
@@ -42,7 +50,7 @@ export const { Provider, Context } = createDataContext(
     authReducer,
     { signup, signin, signout },
     { 
-        isSignedIn: false,
+        token: null,
         errorMessage: ''
     }
 )
